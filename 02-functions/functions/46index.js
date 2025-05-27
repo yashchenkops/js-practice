5651ee3b80286110eb9a5d3759d04d502b6cb57b
Pavel <yashchenkodev@gmail.com>
2025-05-27 22:58:07 +0300

    done 46 homework

--- 02-functions/functions/46index.js
+++ 02-functions/functions/46index.js
@@ -1,26 +1,47 @@
 // higher order functions
 // callback functions
-function numSquared (num) {
-  return num * num;
-}
+// function numSquared (num) {
+//   return num * num;
+// }
+//
+// function copyArrayAndDoSmth (arr, instructions) {
+//   const output = [];
+//
+//   for (let i = 0; i < arr.length; i++) {
+//     output.push(instructions(arr[i]));
+//   }
+//
+//   return output;
+// }
+//
+// function numSqueared (num) {
+//   return num * num;
+// }
+//
+// function divideByTwo (num) {
+//   return num / 2;
+// }
+//
+// const result = copyArrayAndDoSmth([1, 2, 3], numSquared);
+// const result2 = copyArrayAndDoSmth([10, 20, 30], divideByTwo);
 
-function copyArrayAndDoSmth (arr, instructions) {
-  const output = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    output.push(instructions(arr[i]));
+// HOMEWORK
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
   }
+  return newArray;
+}
+const numbers = [1, 2, 3, 4, 5];
 
-  return output;
+function isOdd(num) {
+  return num % 2 !== 0
+}
+function isEven(num) {
+  return num % 2 === 0;
 }
 
-function numSqueared (num) {
-  return num * num;
-}
-
-function divideByTwo (num) {
-  return num / 2;
-}
-
-const result = copyArrayAndDoSmth([1, 2, 3], numSquared);
-const result2 = copyArrayAndDoSmth([10, 20, 30], divideByTwo);
+// Для проверки используйте логи (раскомментируйте их после написания функций)
+console.log(filterArray(numbers, isEven)); // Должен вывести: [2, 4]
+console.log(filterArray(numbers, isOdd)); // Должен вывести: [1, 3, 5]
